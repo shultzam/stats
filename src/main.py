c3d1adb0c9ab2347c7af4a7d8a9ae32bb6ca3288
Allen S <allenmshultz@gmail.com>
2021-04-09 15:05:29 -0400

    added function definitions and basic comment placeholders

---
+++ src/main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+''' TODO: DESCRIPTION
+
+TODO: DETAILS
+'''
+def addAction():
+    print('TEMP | entered tst_addAction()')
+    return
+
+''' TODO: DESCRIPTION
+    
+TODO: DETAILS
+'''
+def getStats():
+    print('TEMP | entered tst_getStats()')
+    return
---
+++ test/tst_main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+''' TODO: DESCRIPTION
+
+TODO: DETAILS
+'''
+def tst_addAction():
+    print('TEMP | entered tst_addAction()')
+    return
+
+''' TODO: DESCRIPTION
+    
+TODO: DETAILS
+'''
+def tst_getStats():
+    print('TEMP | entered tst_getStats()')
+    return
+
+# Entry point.
+if __name__ == "__main__":
+    tst_addAction()
+    tst_getStats()
