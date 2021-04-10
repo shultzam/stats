15f825a30a08861f53f30c166705a838da998d0b
Allen S <allenmshultz@gmail.com>
2021-04-09 20:37:50 -0400

    implemented addAction()

--- stats.py
+++ stats.py
@@ -1,20 +1,54 @@
 #!/usr/bin/python3
+# Created using Python 3.7.5
 
-# Standard Python3 imports.
+# Standard Python3 import(s).
 import json
 
-''' TODO: DESCRIPTION
 
-TODO: DETAILS
-'''
-def addAction(action: str) -> bool:
-    print('TEMP | in action getAction(), received action: {}'.format(json.loads(action)))
-    return True
-
-''' TODO: DESCRIPTION
+''' Stats class '''
+class Stats:
+    def __init__(self):
+        ''' actionDict, a standard Python3 dictionary.
+            
+            Used to maintain incoming data through concurrent calls. The data is stored as a list
+            containing [action count, total duration]. Total count represents the total count of 
+            that specific action given.
+        '''
+        self.actionDict = {}
+        
     
-TODO: DETAILS
-'''
-def getStats() -> str:
-    print('TEMP | entered tst_getStats()')
-    return 'Not yet implemented'
+    ''' TODO: DESCRIPTION
+
+    TODO: DETAILS
+    '''
+    def addAction(self, action: str) -> bool:
+        # Load the JSON into a string. In this case, loads() is really only used to verify JSON format.
+        try:
+            actionJson = json.loads(action)
+            actionName = actionJson['action']
+            actionTime = actionJson['time']
+            #print('TEMP | in action getAction(), received action: {}'.format(actionJson))
+        except TypeError:
+            print('ERROR in addAction(): action ({}) cannot be processed.'.format(action))
+            return False
+        
+        # Save off the action's info for easier handling.
+        
+        # This check prevents a KeyError in the event this is the first of this action type received.
+        if actionName not in self.actionDict:
+            self.actionDict[actionName] = [0, 0]
+            
+        # Add the given action data to the actionDict.
+        self.actionDict[actionName][0] += 1             # Increment action count.
+        self.actionDict[actionName][1] += actionTime    # Increment action duration.
+        
+        return True
+
+    
+    ''' TODO: DESCRIPTION
+
+    TODO: DETAILS
+    '''
+    def getStats(self) -> str:
+        print('TEMP | entered tst_getStats()')
+        return 'Not yet implemented'
--- tst_stats.py
+++ tst_stats.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+# Created using Python 3.7.5
 
 # Standard Python3 imports.
 import json
@@ -6,29 +7,32 @@ import sys
 
 # Import the stats module from the adjacent src directory.
 sys.path.append('../src/')
-from stats import addAction, getStats
+from stats import Stats
 
 ''' TODO: DESCRIPTION
 
 TODO: DETAILS
 '''
 def tst_addAction():
+    # Create class instance.
+    statsObj = Stats()
+    
     # Create some test data and call the addAction function.
     testData  = json.dumps({ 'action': 'jump', 'time': 100 })
-    assert(addAction(testData) == True)
+    assert(statsObj.addAction(testData) == True)
     
     # Repeat the above step with additional data.
     testData = json.dumps({ 'action': 'run', 'time': 75 })
-    assert(addAction(testData) == True)
+    assert(statsObj.addAction(testData) == True)
     
     # Repeat the above step with additional data.
     testData = json.dumps({ 'action': 'jump', 'time': 200 })
-    assert(addAction(testData) == True)
+    assert(statsObj.addAction(testData) == True)
     
-    # Test addAction() against empty data and malformed data.
-    assert(addAction({}) == False)
-    assert(addAction(44) == False)
-    assert(addAction(True) == False)
+    # Test addAction() against empty and malformed data.
+    assert(statsObj.addAction({}) == False)
+    assert(statsObj.addAction(44) == False)
+    assert(statsObj.addAction(True) == False)
     
     return
 
